28d6ac20cd09208b87ef1bdf6130dea233da1514
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-20 18:56:37 +0700

    package.json ลองเปลี่ยนหน้าแรก http://localhost:3000/ เป็น http://localhost:3000/index.html

--- djreact/urls.py
+++ djreact/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('articles.api.urls')),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
