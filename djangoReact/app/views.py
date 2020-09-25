dfbfdb554d1c95a966c3744d85d6be9330e79b46
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-09-25 21:03:50 +0700

    จบ doc ที่ 2 ของโชกุน

---
+++ djangoReact/app/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+import os
+import logging
+from django.conf import settings
+
+# Create your views here.
+
+index_file_path = os.path.join(settings.REACT_APP_DIR, 'build',
+                               'index.html')
+
+
+def index(request):
+    return HttpResponse('Django example')
+
+
+def react(request):
+    try:
+        with open(index_file_path) as f:
+            return HttpResponse(f.read())
+    except FileNotFoundError:
+        logging.exception('Production build of app not found')
