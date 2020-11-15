3b004cd082bb0a960ea4f96f7191211aea792b9d
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-11-15 16:26:24 +0700

    edit "settings.py" ลองแก้ BASE_DIR

--- djangoReact/settings.py
+++ djangoReact/settings.py
@@ -14,8 +14,12 @@ import os
 
 import dj_database_url
 
+from pathlib import Path
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
