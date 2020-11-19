3d02b58c6e9fa6f173d10d20795807540ff1ba1e
dewqwer <natcha.1@mail.kmutt.ac.th>
2020-11-19 19:46:18 +0700

    ลองให้ html แสดงค่าใน <script> ไม่ import  JS จากข้างนอก

---
+++ app/static/app/testJS.js
@@ -0,0 +1,26 @@
+// fetch('http://127.0.0.1:8000/api-web/Major/')
+//     .then(response => {
+//         return response.json();
+//     })
+
+//     .then(majors => {
+//         console.log(majors);
+//     })
+
+
+// class Love {
+//     constructor(a, b) {
+//         this.a = a;
+//         this.b = b;
+//     }
+//     calculated() {
+//         // return this.a + this.b
+//         return this.a;
+
+//     }
+// }
+// const p = new Love('EIEI', 'LOVE');
+// p.calculated();
+
+
+
